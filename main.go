b2b3fb0fd5991655e1c5ebfd83c60aa89b868906
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-11-16 19:54:52 +0530

    feat: add update and view transaction in real-time db PFOC

--- main.go
+++ main.go
@@ -14,9 +14,42 @@ func main() {
 	}
 	defer db.Close()
 
+	data := map[string]string{
+		"Name": "Bharat Panigrahi",
+		"Age": "21",
+	}
+
 	db.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucket("users")
+		bucket, err := tx.CreateBucket([]byte("users"))
+		if err != nil {
+			return err
+		}
+		for k, v := range data {
+			if err := bucket.Put([]byte(k), []byte(v)); err != nil {
+				return err
+			}
+		}
+		return nil
 	})
+
+
+	user := make(map[string]string)
+
+	if err := db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("users"))
+		if bucket == nil {
+			return fmt.Errorf("bucket (%s) not found", "users")
+		}
+
+		bucket.ForEach(func(k, v []byte) error {
+			user[string(k)] = string(v)
+			return nil
+		})
+
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("Hello world")
 }
