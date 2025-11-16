88859ed69b028ad570109fcdf339de9634d621bd
Bharat1056 <bharatpanigrahi225@gmail.com>
2025-11-17 00:43:53 +0530

    feat: create db instantiate and collection create method

--- cmd/main.go
+++ cmd/main.go
@@ -4,52 +4,29 @@ import (
 	"fmt"
 	"log"
 
-	bolt "go.etcd.io/bbolt"
+	"github.com/Bharat1056/real_time_db/photon"
 )
 
 func main() {
-	db, err := bolt.Open(".db", 0666, nil)
+
+	// user := map[string]string{
+	// 	"Name": "Bharat Panigrahi",
+	// 	"Age": "21",
+	// }
+
+
+	db, err := photon.New()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
-	data := map[string]string{
-		"Name": "Bharat Panigrahi",
-		"Age": "21",
-	}
+	coll, err := db.CreateCollection("users")
 
-	db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucket([]byte("users"))
-		if err != nil {
-			return err
-		}
-		for k, v := range data {
-			if err := bucket.Put([]byte(k), []byte(v)); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
-
-	user := make(map[string]string)
-
-	if err := db.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte("users"))
-		if bucket == nil {
-			return fmt.Errorf("bucket (%s) not found", "users")
-		}
-
-		bucket.ForEach(func(k, v []byte) error {
-			user[string(k)] = string(v)
-			return nil
-		})
-
-		return nil
-	}); err != nil {
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(user)
+	fmt.Printf("%+v\n", coll)
+
 }
--- photon/photon.go
+++ photon/photon.go
@@ -10,7 +10,9 @@ const (
 	defaultDbName = "default"
 )
 
-type Collection struct {}
+type Collection struct {
+	bucket *bolt.Bucket
+}
 
 type Photon struct {
 	db *bolt.DB
@@ -28,5 +30,36 @@ func New() (*Photon, error) {
 }
 
 func (p *Photon) CreateCollection(name string) (*Collection, error) {
-
+	coll := Collection{}
+	err := p.db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucket([]byte("users"))
+		if err != nil {
+			return  err
+		}
+		coll.bucket = bucket
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &coll, nil
 }
+
+
+	// user := make(map[string]string)
+
+	// if err := db.View(func(tx *bolt.Tx) error {
+	// 	bucket := tx.Bucket([]byte("users"))
+	// 	if bucket == nil {
+	// 		return fmt.Errorf("bucket (%s) not found", "users")
+	// 	}
+
+	// 	bucket.ForEach(func(k, v []byte) error {
+	// 		user[string(k)] = string(v)
+	// 		return nil
+	// 	})
+
+	// 	return nil
+	// }); err != nil {
+	// 	log.Fatal(err)
+	// }
